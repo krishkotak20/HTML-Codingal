66e8d25d775b60583c12826834d1d38b55bc4f3c
krishkotak20 <krishkotak20@gmail.com>
2026-05-17 14:24:40 +0400

    hi

---
+++ script1.js
@@ -0,0 +1,56 @@
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+    document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+    if(num==""){
+        document.getElementById("output-value").innerText=num;
+    }
+    else{document.getElementById("output-value").innerText=getFormattedNumber(num);
+
+    }
+}
+function getFormattedNumber(num){
+    if(num=="-"){
+        return "";
+    }
+    var n=Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+function reversedNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i=0;i<operator.length;i++){
+    operator[i].addEventListener('click',function(){
+        if(this.id=='clear'){
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id=="backspace"){
+            var output=reversedNumberFormat(getOutput()).toString();
+            if(output){
+                output= output.substr(0,output.length-1);
+                printOutput(output);
+                
+            }
+        }
+        else{
+            var output=getOutput();
+            var history=getHistory();
+            if(output==""&&history!=""){
+                if(isNaN(history[history.length-1]))
+                    history= history+output;
+                if(this.id=="="){
+                    var result=eval(history)
+                }
+            }
+        }
+    }
+}
