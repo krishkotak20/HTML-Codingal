dfc0e68ef5cf9be77a0227c519b0f9ff7830b0c5
krishkotak20 <krishkotak20@gmail.com>
2026-06-07 14:19:37 +0400

    hi

---
+++ python/ac1lesson2.py
@@ -0,0 +1,6 @@
+def intro(name):
+    print("hello,my name is", name)
+
+
+user_name = input("enter your name")
+intro(user_name)
---
+++ python/extra.py
@@ -0,0 +1,5 @@
+def sample_fun():
+    a = 5
+    print("this is a sample function...")
+    return a
+print(sample_fun())
