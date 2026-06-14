69f572d446cb0cada2fce205fbd02654b5d05d8f
krishkotak20 <krishkotak20@gmail.com>
2026-06-14 14:30:05 +0400

    hi

--- python/turtlerectangle.py
+++ python/turtlerectangle.py
@@ -3,9 +3,15 @@ turtle.Screen().bgcolor("orange")
 board=turtle.Turtle()
 
 board.forward(120)
-board.right(80)
+board.right(90)
+
+board.forward(100)
+board.right(90)
 
 board.forward(120)
-board.left(80)
+board.right(90)
+
+board.forward(100)
+board.right(90)
 
 turtle.done()
